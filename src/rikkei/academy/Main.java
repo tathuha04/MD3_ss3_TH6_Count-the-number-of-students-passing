27c5b3d72347f724527686dc52868dc0cb3c6456
TaThuHa <thuhak2tltnpt@gmail.com>
2023-03-02 19:14:40 +0700

    up

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,33 @@
+package rikkei.academy;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        int[] array;
+        do {
+            System.out.println("enter a size");
+            size = scanner.nextInt();
+            if (size > 30) {
+                System.out.println("size should not exceed 30");
+            }
+        } while (size > 30);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.println("enter a mark for student " + (i + 1) + " :");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        int count = 0;
+        System.out.println("list of mark: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + " ");
+            if (array[j] >= 5 && array[j] <= 10)
+                count++;
+        }
+        System.out.println(".The number of students passing the exam is: " + count);
+    }
+}
